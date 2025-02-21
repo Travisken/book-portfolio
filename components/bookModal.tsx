0ce3aca52053641a35532d2e5c09f25717770df8
Franklyn101 <franklyninoma@gmail.com>
2025-02-21 15:23:06 +0100

    ssssssssssssss

--- components/bookModal.tsx
+++ components/bookModal.tsx
@@ -16,7 +16,8 @@ interface BookModalProps {
         image: string;
         aboutBook: string;
         title: string;
-        bookDocument: string;
+        // bookDocument: string;
+        bookLink: string;
         rating: number;
 
     } | null;
@@ -47,8 +48,8 @@ const BookModal: React.FC<BookModalProps> = ({ open, onClose, book }) => {
     const router = useRouter();
 
 const handleReadNow = () => {
-    if (book?.bookDocument) {
-        router.push(`/pdf-viewer?bookDocument=${encodeURIComponent(book.bookDocument)}`);
+    if (book?.bookLink) {
+        router.push(`/pdf-viewer?bookDocument=${encodeURIComponent(book.bookLink)}`);
     }
 };
 
@@ -82,7 +83,7 @@ const handleReadNow = () => {
         const templateParams = {
             user_email: formData.email,
             book_bookName: book.bookName,
-            book_link: book.bookDocument,
+            book_link: book.bookLink,
         };
 
         try {
@@ -95,7 +96,7 @@ const handleReadNow = () => {
 
             console.log("Email sent successfully:", response);
             setSuccess("Email sent! Check your inbox.");
-                router.push(`/pdf-viewer?bookDocument=${encodeURIComponent(book.bookDocument)}`);
+                router.push(`/pdf-viewer?bookDocument=${encodeURIComponent(book.bookLink)}`);
             setFormData({ email: "" });
         } catch (error) {
             console.error("Failed to send email:", error);
