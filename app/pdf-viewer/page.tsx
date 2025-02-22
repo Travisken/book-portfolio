f9d8d1bdfdb04bba7a137ece517d6cc5148b3cbf
ken travis <kentravis37@gmail.com>
2025-02-22 14:49:08 +0100

    update

--- app/pdf-viewer/page.tsx
+++ app/pdf-viewer/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import PdfViewer from "@/components/pdfViewer";
+import { Suspense } from "react";
 
 const BookPage = () => {
   return (
   <>  
+  <Suspense fallback={<p className="text-center">Loading PDF...</p>}>
   <PdfViewer />
+  </Suspense>
   </>
 );
 };
