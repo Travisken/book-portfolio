19d70ad7014d03bf37ac571c82b442cb631a9222
ken travis <kentravis37@gmail.com>
2025-03-29 10:30:38 +0100

    update

--- components/testimonialCard.tsx
+++ components/testimonialCard.tsx
@@ -1,4 +1,5 @@
 import { Star } from "lucide-react";
+import { useState, useEffect } from "react";
 
 interface TestimonialProps {
   fullName: string;
@@ -19,11 +20,26 @@ const TestimonialCard: React.FC<TestimonialProps> = ({
   isExpanded,
   onToggle,
 }) => {
+
+  const [name, setName] = useState("");
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    if (fullName.includes("Will Katz")) {
+      const parts = fullName.split(", ");
+      setName(parts[0]); // First part as name
+      setTitle(parts.slice(1).join(", ")); // Remaining parts as title
+    } else {
+      setName(fullName);
+      setTitle("");
+    }
+  }, [fullName]);
+
   return (
     <div
       className={`bg-white flex flex-col gap-4 text-center mx-auto border md:shadow-lg rounded-2xl p-2 md:p-6  transition-all duration-500 ease-in-out 
         ${isExpanded ? "w-[100%] md:w-[50vw] z-10 scale-100" : "w-[100%] md:w-[32%] scale-100 opacity-70"}
-        ${isExpanded ? "" : "h-auto md:h-[18rem] overflow-hidden"} `}
+        ${isExpanded ? "" : "h-auto md:h-[19rem] overflow-hidden"} `}
       onClick={onToggle}
     >
       <p className={`text-gray-700 mt-4 italic ${isExpanded ? "" : "line-clamp-3"}`}>
@@ -44,7 +60,8 @@ const TestimonialCard: React.FC<TestimonialProps> = ({
       </div>
 
       <div>
-        <h3 className="text-lg font-semibold whitespace-normal break-words">{fullName}</h3>
+        <h3 className="text-xl font-semibold whitespace-normal break-words">{name}</h3>
+        {title && <p className="text-base font-medium text-gray-600">{title}</p>}
         <p className="text-sm text-gray-500">{email}</p>
       </div>
     </div>
